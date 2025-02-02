04f66a8f2235f51b64b88f629368ed8a3b5ba04f
Davut Simsek <davutt.simsek@gmail.com>
2025-02-02 13:11:20 +0100

    implement scrolling with scrollIntoView()

--- script.js
+++ script.js
@@ -31,4 +31,43 @@ document.addEventListener('keydown', function (e) {
 });
 ///////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////// IMPLEMENT SMOOTH SCROLLING(scrollIntoView() function)
+const btnSCrollTo = document.querySelector(`.btn--scroll-to`);
+const section1 = document.querySelector(`#section--1`);
+btnSCrollTo.addEventListener(`click`, function (e) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////
+// // Styles
+// document.documentElement.style.setProperty(`--color-primary`, `orangered`);
+// // Attributes
+// const logo = document.querySelector(`.nav__logo`);
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.id);
+// console.log(logo.className);
+
+// logo.alt = `Beautiful minimalist logo`;
+
+// // Get and Set Attributes
+// console.log(logo.getAttribute(`id`));
+// logo.setAttribute(`company`, `Bankist`);
+// console.log(logo.getAttribute(`src`));
+
+// const link = document.querySelector(`.nav__link--btn`);
+// console.log(link.href);
+// console.log(link.getAttribute(`href`));
+
+// // Data Attribute
+// console.log(logo.dataset.versionNumber);
+
+// // Classes
+// logo.classList.add(`c`);
+// logo.classList.remove(`c`);
+// logo.classList.toggle(`c`);
+// logo.classList.contains(`c`);
+
+// // Don't use override all other classes
+// // logo.className = `dav`
